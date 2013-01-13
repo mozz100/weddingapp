48310e35427029001ebbe6868f9587d0aecb62f2
Richard Morrison <richard@rmorrison.net>
2013-01-13 20:23:04 +0000

    Adjust seed data and choose a better name for the layout

--- config/initializers/refinery/pages.rb
+++ config/initializers/refinery/pages.rb
@@ -39,7 +39,7 @@ Refinery::Pages.configure do |config|
   # Set this to true to fully expand the page hierarchy in the admin
   # config.auto_expand_admin_tree = true
 
-  config.layout_template_whitelist = ["layout"]
+  config.layout_template_whitelist = ["bootstrap"]
 
   # config.view_template_whitelist = ["home", "show"]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,36 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Added by Refinery CMS Pages extension
-Refinery::Pages::Engine.load_seed
+# Refinery::Pages::Engine.load_seed
+if Refinery::Page.where(:menu_match => "^/$").empty?
+  home_page = Refinery::Page.create!({:title => "Home",
+              :layout_template => "bootstrap",
+              :deletable => false,
+              :link_url => "/",
+              :show_in_menu => false,
+              :menu_match => "^/$"})
+  home_page.parts.create({
+                :title => "Body",
+                :body => "<p>Welcome to our site. This is just a place holder page while we gather our content.</p>",
+                :position => 0
+              })
+  home_page.parts.create({
+                :title => "Side Body",
+                :body => "<p>This is another block of content over here.</p>",
+                :position => 1
+              })
+
+  home_page_position = -1
+  page_not_found_page = home_page.children.create(:title => "Page not found",
+              :layout_template => "bootstrap",
+              :menu_match => "^/404$",
+              :show_in_menu => false,
+              :deletable => false)
+  page_not_found_page.parts.create({
+                :title => "Body",
+                :body => "<h2>Sorry, there was a problem...</h2><p>The page you requested was not found.</p><p><a href='/'>Return to the home page</a></p>",
+                :position => 0
+              })
+
+end
+
