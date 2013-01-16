2fd53592f600f3a5f969c54990a6cae1a3af3546
Richard Morrison <richard@rmorrison.net>
2013-01-16 23:06:57 +0000

    Take account of html_escape in integration test

--- test/integration/initial_pages_test.rb
+++ test/integration/initial_pages_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class InitialPagesTest < ActionDispatch::IntegrationTest
+  include ERB::Util
   test "home page" do
     get "/"
     assert_response :success
     # <title> should be "Home - (whatever's configured)"
-    assert_select "title", "#{Refinery::Page.find_by_link_url("/").title} - #{Rails.application.config.wedding_name}"
+    assert_select "title", html_escape("#{Refinery::Page.find_by_link_url("/").title} - #{Rails.application.config.wedding_name}")
   end
 end
