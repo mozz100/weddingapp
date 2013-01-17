ffb46120f5a72ac67a1bfdbc400610e8e699cb3f
Richard Morrison <richard@rmorrison.net>
2013-01-17 22:04:51 +0000

    More tests

--- test/integration/initial_pages_test.rb
+++ test/integration/initial_pages_test.rb
@@ -2,10 +2,22 @@ require 'test_helper'
 
 class InitialPagesTest < ActionDispatch::IntegrationTest
   include ERB::Util
+
+  config = Rails.application.config
+
   test "home page" do
     get "/"
     assert_response :success
     # <title> should be "Home - (whatever's configured)"
-    assert_select "title", html_escape("#{Refinery::Page.find_by_link_url("/").title} - #{Rails.application.config.wedding_name}")
+    assert_select "title", html_escape("#{config.home_page[:title]} - #{config.wedding_name}")
+
+    get "/" + config.rsvp_page[:slug]
+    assert_response :success
+    assert_select "title", html_escape("#{config.rsvp_page[:title]} - #{config.wedding_name}")
+  end
+
+  test "rsvp form" do
+    get "/" + config.rsvp_page[:slug]
+    assert css_select("section#rsvp_form")
   end
 end
