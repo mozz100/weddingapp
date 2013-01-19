48c10fdbed927bd26ea62f3f8f702ee47a8e0102
Richard Morrison <richard@rmorrison.net>
2013-01-19 00:51:04 +0000

    Start functional tests

--- test/functional/guests_controller_test.rb
+++ test/functional/guests_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class GuestsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "rsvp search" do
+      post :search, {:rsvp_code => guests(:alan).rsvp_code}
+      assert_redirected_to :action => :show, :rsvp_code => guests(:alan).rsvp_code
+      assert_equal assigns(:guest), guests(:alan)
+  end
 end
