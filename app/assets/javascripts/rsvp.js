d42862a593bee5389fada09495489aebe0fdd771
Richard Morrison <richard@rmorrison.net>
2013-01-22 08:53:17 +0000

    Better js logic

--- app/assets/javascripts/rsvp.js
+++ app/assets/javascripts/rsvp.js
@@ -1,13 +1,26 @@
 $(function() {
-    $('.custom_questions').hide();
-    $('.are_you_coming .radio').click(function() {
-        var coming = $(this).find('input:checked').val() > 0;
-        if (coming) {
+    function guest_is_coming() {
+        return $('input:radio[name="guest[status]"]:checked').val() == 1;
+    }
+
+    function set_visibility_based_on_rsvp() {
+        if (guest_is_coming()) {
             $('.notes .question').text(notes_coming);
             $('.custom_questions').slideDown();
         } else {
             $('.notes .question').text(notes);
             $('.custom_questions').slideUp();
         }
-    })
+    }
+
+    // start up behaviour
+    if (!guest_is_coming()) {
+        $('.custom_questions').hide();
+    } else {
+        // do nothing
+    }
+    
+    $('.are_you_coming .radio').click(function() {
+        set_visibility_based_on_rsvp();
+    });
 })
